05ae07c7e45827811feb4dba81a5db0878955204
Arthur Mois <arthurmois@gmail.com>
2022-01-20 23:30:14 -0800

    temp solution, initial useState

--- pages/MapChart.js
+++ pages/MapChart.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import getISSLocation from "./api/location";
 import {
   ComposableMap,
@@ -11,18 +12,38 @@ import {
 var longitude;
 var latitude;
 
-async function getLocation() {
-  let data = await getISSLocation();
+// async function getLocation() {
+//   let data = await getISSLocation();
 
-  longitude = data.iss_position.longitude;
-  latitude = data.iss_position.latitude;
-}
+//   longitude = data.iss_position.longitude;
+//   latitude = data.iss_position.latitude;
+// }
+
+
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
   const MapChart = () => {
-    getLocation()
+    const [data,setData] = useState({"iss_position": {"latitude": "29.4752", "longitude": "25.1402"}});
+    useEffect(() => {
+      // declare the async data fetching function
+      const fetchData = async () => {
+        // get the data from the api
+        const response = await fetch('http://api.open-notify.org/iss-now.json');
+        console.log(JSON.stringify(data));
+        // convert the data to json
+        const json = await response.json();
+    
+        // set state with the result
+        setData(json);
+      }
+    
+      // call the function
+      fetchData()
+        // make sure to catch any error
+        .catch(console.error);;
+    }, [])
   return (
     <div>
       <ComposableMap>
@@ -34,7 +55,7 @@ const geoUrl =
               ))
             }
           </Geographies>
-          <Marker coordinates={[longitude, latitude]}>
+          <Marker coordinates={[data.iss_position.longitude, data.iss_position.longitude]}>
         <circle r={6} fill="#2ECCE7" />
       </Marker>
         </ZoomableGroup>
