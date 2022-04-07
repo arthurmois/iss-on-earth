f4470695fb88ae8dfdfd67486ad93914c94f7811
Arthur Mois <arthurmois@gmail.com>
2022-04-06 21:26:38 -0700

    css final touches

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
-import styles from '../styles/Home.module.css'
-import getISSLocation from './api/location'
+import styles from "../styles/Home.module.css";
+import getISSLocation from "./api/location";
 import MapChart from "./MapChart";
 
 export default function Home() {
   return (
-  <>
-    <h1 style={{margin:'20px', textAlign: "center"}}>Where is the International Space Station</h1>
-    <div style={{marginLeft:'20px',marginRight:'20px', borderStyle: 'solid'}}>
+    <>
+      <h1 style={{ margin: "20px", textAlign: "center" }}>
+        Where is the International Space Station
+      </h1>
+      <div
+        style={{
+          marginLeft: "40px",
+          marginRight: "40px"
+        }}
+      >
+        <div
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: "1000px",
+            borderStyle: "solid",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <MapChart />
         </div>
+      </div>
 
       <footer className={styles.footer}>
         <a
           href="https://www.linkedin.com/in/arthurmois/"
           target="_blank"
           rel="noopener noreferrer"
+          style={{textDecoration: 'underline'}}
         >
           Created by Arthur Mois
         </a>
       </footer>
-  </>
-  )
+    </>
+  );
 }
